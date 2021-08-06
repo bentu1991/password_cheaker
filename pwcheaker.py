a30ca1a43f27630deb763fd9be7b44c367d3ba08
Ben Tu <bentu1991@gmail.com>
2021-08-06 13:29:41 -0700

    verison 2

--- pwcheaker.py
+++ pwcheaker.py
@@ -1,15 +1,14 @@
 pw = '123456'
 i = 3
-while True:
+while i > 0:
 	pwenter = input('please enter your password: ')
 	if pwenter == pw:
 		print('log in success')
 		break
 	else:
 		i = i - 1
-		print('wrong password, you have', i,  'attempts')
-		if i == 0:
-			break
+		print('wrong password, you still have', i,  'attempts')
+
 			
 			
 	
